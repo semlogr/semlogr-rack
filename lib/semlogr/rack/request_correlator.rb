0c2cee233b7e81165bd572689243910c2a0d2f05
Stefan Sedich <stefan.sedich@gmail.com>
2018-04-02 17:58:36 -0700

    Adding request correlator to support propograting a correlation id via the Semlogr ambient context (#1)
    Adding request correlator to support propagating a correlation id via the Semlogr ambient log context

---
+++ lib/semlogr/rack/request_correlator.rb
@@ -0,0 +1,33 @@
+require 'semlogr'
+require 'securerandom'
+
+module Semlogr
+  module Rack
+    class RequestCorrelator
+      def initialize(app, opts = {})
+        @app = app
+        @id_header = opts[:id_header] || 'X-Correlation-Id'
+        @id_generator = opts[:id_generator] || -> { SecureRandom.uuid }
+      end
+
+      def call(env)
+        id_header = env_header_name(@id_header)
+        correlation_id = env[id_header] || @id_generator.call
+        status, headers, body =
+          Semlogr::Context::LogContext.push_property(correlation_id: correlation_id) do
+            @app.call(env)
+          end
+
+        headers[@id_header] = correlation_id
+
+        [status, headers, body]
+      end
+
+      private
+
+      def env_header_name(header)
+        'HTTP_' + header.tr('-', '_').upcase
+      end
+    end
+  end
+end
---
+++ spec/semlogr/rack/request_correlator_spec.rb
@@ -0,0 +1,67 @@
+require 'semlogr'
+require 'semlogr/rack/request_correlator'
+
+module Semlogr
+  module Rack
+    describe RequestCorrelator do
+      let(:env_without_id) { {} }
+      let(:env_with_id) { { 'HTTP_X_CORRELATION_ID' => '1234' } }
+      let(:app) { spy('app') }
+      let(:id_generator) { spy('id_generator') }
+
+      subject { RequestCorrelator.new(app, id_generator: id_generator) }
+
+      before do
+        allow(app)
+          .to receive(:call)
+          .and_return([200, {}, 'Hello'])
+
+        allow(Semlogr::Context::LogContext)
+          .to receive(:push_property)
+          .and_yield
+      end
+
+      context 'with x-correlation-id header' do
+        let(:correlation_id) { env_with_id['HTTP_X_CORRELATION_ID'] }
+
+        it 'adds existing correlation-id to the log context' do
+          subject.call(env_with_id)
+
+          expect(Semlogr::Context::LogContext)
+            .to have_received(:push_property)
+            .with(correlation_id: correlation_id)
+        end
+
+        it 'adds correlation-id to response headers' do
+          _status, headers, _body = subject.call(env_with_id)
+
+          expect(headers['X-Correlation-Id']).to eq(correlation_id)
+        end
+      end
+
+      context 'without x-correlation-id header' do
+        let(:correlation_id) { '4321' }
+
+        before do
+          allow(id_generator)
+            .to receive(:call)
+            .and_return(correlation_id)
+        end
+
+        it 'adds new correaltion-id to the log context' do
+          subject.call(env_without_id)
+
+          expect(Semlogr::Context::LogContext)
+            .to have_received(:push_property)
+            .with(correlation_id: correlation_id)
+        end
+
+        it 'adds correlation-id to response headers' do
+          _status, headers, _body = subject.call(env_without_id)
+
+          expect(headers['X-Correlation-Id']).to eq(correlation_id)
+        end
+      end
+    end
+  end
+end
